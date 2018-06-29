92b9af0af732abb0667a933c67d19027bc9a0177
Uday Kumar Reddy Pamulapati <4715599+uday768@users.noreply.github.com>
2018-06-29 12:06:45 +0530

    Create AccountRepository.java

---
+++ src/main/java/com/suncorp/banking/application/modifiedfiles/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.suncorp.banking.application.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.suncorp.banking.application.vo.AccountVo;
+
+@Repository
+public interface AccountRepository extends JpaRepository<AccountVo, Long> {
+	
+	AccountVo findAccountByAccountnumber(String accountNumber);
+}
