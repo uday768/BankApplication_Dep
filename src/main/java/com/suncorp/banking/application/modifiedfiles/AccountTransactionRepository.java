44520563f592e6ade7acad80c6731e1f0a7d1d06
Uday Kumar Reddy Pamulapati <4715599+uday768@users.noreply.github.com>
2018-06-29 12:07:05 +0530

    Create AccountTransactionRepository.java

---
+++ src/main/java/com/suncorp/banking/application/modifiedfiles/AccountTransactionRepository.java
@@ -0,0 +1,15 @@
+package com.suncorp.banking.application.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.suncorp.banking.application.vo.AccountTransactionVO;
+
+@Repository
+public interface AccountTransactionRepository extends JpaRepository<AccountTransactionVO, Long> {
+
+	List<AccountTransactionVO> findByAccountId(Long accountId);
+
+}
